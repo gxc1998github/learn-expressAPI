49411bc281676d58045e9e30f702a1ee533be8fc
Daniel Lip <116948520+gxc1998github@users.noreply.github.com>
2023-08-12 10:14:42 +1200

    make  it successs to localhost:3000/api/books/test

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 // Add Express
 const express = require("express");
+
+//  Add books
+const books = require("./api/books");
+
 // Initialize Express
 const app = express();
 // Create GET request
 app.get("/", (req, res) => {
   res.send("Express API on Vercel 🪐");
 });
+
+//  use the made api
+app.use('/api/books', books);
+
+
 // Initialize server
 app.listen(3000, () => {
   console.log("Running on port 3000.");
@@ -13,3 +22,4 @@ app.listen(3000, () => {
 
 // Export the Express API
 module.exports = app;
+
