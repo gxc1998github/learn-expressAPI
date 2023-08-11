5ca704d24acad10473ade343490c294608362e97
Daniel Lip <116948520+gxc1998github@users.noreply.github.com>
2023-08-12 09:29:41 +1200

    add vercel json

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 // Create GET request
 app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+  res.send("Express API on Vercel 🪐");
 });
 // Initialize server
 app.listen(3000, () => {
